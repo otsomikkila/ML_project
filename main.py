e90af44c22bb1d946283d3ea925c4db3bbcdb819
Reko Raitasalo <reko.raitasalo@aalto.fi>
2025-09-17 13:51:47 +0300

    Added a function for getting the data

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2 as cv
-import os  # import os module
+import os
 
-directory = './archive/test'  # set directory path
+def getData(directory):
+  cards = []
+  for entry in os.scandir(directory):
+      if entry.is_dir():  # make sure it's a folder (e.g. 'five of diamonds')
+          for image in os.scandir(entry.path):  # use .path instead of entry
+              if image.name.endswith(('.jpg', '.png', '.jpeg')):  # only images
+                  # Read image with OpenCV
+                  img = cv.imread(image.path)         # loads as BGR NumPy array
+                  img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # convert to RGB (optional)
 
-cards = []
+                  # Convert to NumPy array (though OpenCV already returns np.array)
+                  arr = np.array(img)
 
-for entry in os.scandir(directory):
-    if entry.is_dir():  # make sure it's a folder (e.g. 'five of diamonds')
-        for image in os.scandir(entry.path):  # use .path instead of entry
-            if image.name.endswith(('.jpg', '.png', '.jpeg')):  # only images
-                # Read image with OpenCV
-                img = cv.imread(image.path)         # loads as BGR NumPy array
-                img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # convert to RGB (optional)
+                  cards.append(arr)
+                  print(f"Loaded {image.path} with shape {arr.shape}")
+                  #print(f"Total images loaded: {len(cards)}")
+  return cards
 
-                # Convert to NumPy array (though OpenCV already returns np.array)
-                arr = np.array(img)
+testData = getData('./archive/test')
+validateData = getData('./archive/valid')
+#trainData = getData('./archive/train')
 
-                cards.append(arr)
-                print(f"Loaded {image.path} with shape {arr.shape}")
-
-print(f"Total images loaded: {len(cards)}")
